06740a81d8d309dbbc21531410a46a1915a69fd4
Muhammad Umar <MuhammadUmarSmiles@gmail.com>
2025-05-02 20:04:20 +0500

    Remove unused application function from app.py

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
-
-# def application(environ, start_response): 
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     message = 'It works!\n'
-#     version = 'Python %s\n' % sys.version.split()[0]
-#     response = '\n'.join([message, version])
-#     return [response.encode()]
-
 import yt_dlp
 from flask import Flask, request, render_template, jsonify
 import humanize
